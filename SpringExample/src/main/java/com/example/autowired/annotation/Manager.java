a4eb36d35bb750c2158a5df12c4eeaadec0243f7
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-17 20:29:33 +0530

    sbfs-41-qualifier-annotation

--- SpringExample/src/main/java/com/example/autowired/annotation/Manager.java
+++ SpringExample/src/main/java/com/example/autowired/annotation/Manager.java
@@ -1,11 +1,13 @@
 package com.example.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
     @Autowired
+    @Qualifier("employee")
     private Employee employee;
 
     /*@Autowired
