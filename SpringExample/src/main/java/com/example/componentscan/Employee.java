1bd38d69383a119919ef2efb5c6de7e8049f76ec
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-17 19:58:34 +0530

    sbfs-38-value-annotation

--- SpringExample/src/main/java/com/example/componentscan/Employee.java
+++ SpringExample/src/main/java/com/example/componentscan/Employee.java
@@ -1,12 +1,19 @@
 package com.example.componentscan;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("employee")
 public class Employee {
     private int employeeId;
+
+    @Value("Hello")
     private String firstName;
+
+    @Value("${java.home}")
     private String lastName;
+
+    @Value("#{4*4}")
     private double salary;
 
     public int getEmployeeId() {
