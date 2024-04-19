0c5f1fb6f446e0e6b1399a61e6577f848ae265c2
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-19 13:31:14 +0530

    sbfs-62-setting-up-project

---
+++ sb-ecom/src/main/java/com/ecommerce/project/SbEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomApplication.class, args);
+	}
+
+}
---
+++ sb-ecom/src/test/java/com/ecommerce/project/SbEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
