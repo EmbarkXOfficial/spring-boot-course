f5b249a693bba9f620c65e2c35dbdb4a0ba5f7f9
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-19 12:11:07 +0530

    project copied from spring-micro-0500-introduction-to-application-properties

---
+++ FirstSpring/src/test/java/com/embarkx/FirstSpring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarkx.FirstSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
