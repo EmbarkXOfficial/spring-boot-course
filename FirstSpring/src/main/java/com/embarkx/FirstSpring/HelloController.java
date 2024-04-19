22443f52fb3c28fa5aac0f6b0db87c4ae34ed0b8
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-19 12:41:35 +0530

    Code updated until: spring-micro-0540-introduction-and-setup-for-postman

--- FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -1,13 +1,20 @@
 package com.embarkx.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name) {
+        return "Hello, " + name + "!";
     }
 }
