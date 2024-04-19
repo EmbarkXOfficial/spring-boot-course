16434b871d1f11170076d6e2f1f407c069a36264
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-19 14:22:02 +0530

    sbfs-65-adding-new-categories

--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,5 +17,11 @@ public class CategoryController {
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
         return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category added successfully";
     }
 }
