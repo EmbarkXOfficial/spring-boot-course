402e0227332b4049d88991b4f4dd51dc0c1cb5ca
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-22 15:17:44 +0530

    sbfs-85-generation-types

--- sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,17 +1,23 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
     }
 
     public String getCategoryName() {
