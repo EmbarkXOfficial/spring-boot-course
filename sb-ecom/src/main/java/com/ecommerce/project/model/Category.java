9fe4b3bda9834415153419f2b2095dd2dfcbd13d
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-23 14:23:40 +0530

    sbfs-98-more-validations

--- sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,5 +20,6 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 }
