b4c65b85b9232694200a11f79207d60e5a4dfc9b
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-23 14:11:33 +0530

    sbfs-97-making-user-friendly

---
+++ sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ecommerce.project.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError)err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName,message);
+        });
+        return new ResponseEntity<Map<String,String>>(response,
+                HttpStatus.BAD_REQUEST);
+    }
+}
