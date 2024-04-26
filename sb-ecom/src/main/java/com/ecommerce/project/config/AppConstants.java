cfa2f83fd94f49f456c87f631a73aba98d23a7e9
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-26 20:05:52 +0530

    sbfs-118-adding-default-values

---
+++ sb-ecom/src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -0,0 +1,6 @@
+package com.ecommerce.project.config;
+
+public class AppConstants {
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "50";
+}
--- sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
@@ -18,8 +19,8 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategories(
-            @RequestParam(name = "pageNumber") Integer pageNumber,
-            @RequestParam(name = "pageSize") Integer pageSize) {
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize) {
         CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
