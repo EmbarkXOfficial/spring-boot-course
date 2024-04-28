15b34420c4268a9abcb91430218e00532459612b
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-28 13:17:14 +0530

    sbfs-123-setting-project

---
+++ media/src/main/java/com/social/media/MediaApplication.java
@@ -0,0 +1,13 @@
+package com.social.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaApplication.class, args);
+	}
+
+}
---
+++ media/src/test/java/com/social/media/MediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
