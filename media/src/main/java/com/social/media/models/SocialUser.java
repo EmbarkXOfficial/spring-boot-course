a55460324349272fcd8809023d58951976309110
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-28 14:37:44 +0530

    sbfs-125-bi-directional-mapping

--- media/src/main/java/com/social/media/models/SocialUser.java
+++ media/src/main/java/com/social/media/models/SocialUser.java
@@ -1,13 +1,14 @@
 package com.social.media.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(mappedBy = "user")
+    //@JoinColumn(name = "social_profile_id")
+    private SocialProfile socialProfile;
 }
