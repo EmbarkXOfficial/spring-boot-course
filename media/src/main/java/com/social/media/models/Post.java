74c197f42bf23d57135ff929f6925ef57c7df40c
Faisal Memon <faisal.memon1991@gmail.com>
2024-04-29 13:43:15 +0530

    sbfs-129-circular-references

--- media/src/main/java/com/social/media/models/Post.java
+++ media/src/main/java/com/social/media/models/Post.java
@@ -1,5 +1,6 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,5 +17,6 @@ public class Post {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private SocialUser socialUser;
 }
--- media/src/main/java/com/social/media/models/SocialGroup.java
+++ media/src/main/java/com/social/media/models/SocialGroup.java
@@ -1,5 +1,6 @@
 package com.social.media.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,6 +20,7 @@ public class SocialGroup {
     private Long id;
 
     @ManyToMany(mappedBy = "groups")
+    @JsonIgnore
     private Set<SocialUser> socialUsers = new HashSet<>();
 
     @Override
