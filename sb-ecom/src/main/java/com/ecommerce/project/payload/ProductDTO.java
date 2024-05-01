6f117f0f444223bb97be1186a2532b7c71e3fbff
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-01 10:56:06 +0530

    sbfs-142-dtos-products

---
+++ sb-ecom/src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -0,0 +1,18 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+}
