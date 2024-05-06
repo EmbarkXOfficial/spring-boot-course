2657c141bb5c52b0dc6111d61f9bf11a9d0252cc
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-06 11:42:12 +0530

    sbfs-180-implement-relationships

--- sb-ecom/src/main/java/com/ecommerce/project/model/User.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/User.java
@@ -5,7 +5,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -34,4 +39,13 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+    @Setter
+    @Getter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
