4631a05a8469466b6c1de8f6a3203971e436f1e6
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-06 12:09:54 +0530

    sbfs-181-uniqueconstraints

--- sb-ecom/src/main/java/com/ecommerce/project/model/User.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/User.java
@@ -15,23 +15,31 @@ import java.util.Set;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+        })
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
     @Size(max = 20)
+    @Column(name = "username")
     private String userName;
 
     @NotBlank
     @Size(max = 50)
     @Email
+    @Column(name = "email")
     private String email;
 
     @NotBlank
     @Size(max = 120)
+    @Column(name = "password")
     private String password;
 
     public User(String userName, String email, String password) {
