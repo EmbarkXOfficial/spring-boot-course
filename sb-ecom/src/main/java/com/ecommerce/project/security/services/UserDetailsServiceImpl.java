5561c3f50ac65830adbf158462b12c3f26429f62
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-06 18:53:47 +0530

    sbfs-189-custom-userdetailservice

---
+++ sb-ecom/src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ecommerce.project.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+}
