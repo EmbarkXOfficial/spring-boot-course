3f9e0feb5f6631ba1f60b3d840ff5df3ceb8df8a
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-10 12:27:32 +0530

    sbfs-200-authenticated-user-details

--- sb-ecom/src/main/java/com/ecommerce/project/controller/AuthController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/AuthController.java
@@ -23,10 +23,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -130,5 +127,28 @@ public class AuthController {
         userRepository.save(user);
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @GetMapping("/username")
+    public String currentUserName(Authentication authentication){
+        if (authentication != null)
+            return authentication.getName();
+        else
+            return "";
+    }
+
+
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserDetails(Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        UserInfoResponse response = new UserInfoResponse(userDetails.getId(),
+                userDetails.getUsername(), roles);
+
+        return ResponseEntity.ok().body(response);
     }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ sb-ecom/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -34,7 +34,6 @@ public class JwtUtils {
     public String getJwtFromCookies(HttpServletRequest request) {
         Cookie cookie = WebUtils.getCookie(request, jwtCookie);
         if (cookie != null) {
-            System.out.println("COOKIE: " + cookie.getValue());
             return cookie.getValue();
         } else {
             return null;
