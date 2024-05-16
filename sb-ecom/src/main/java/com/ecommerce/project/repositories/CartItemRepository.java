cf1165f04a61c338d6a9cda6d2a11451b6732219
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-16 21:46:07 +0530

    sbfs-215-testing-changes

--- sb-ecom/src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -2,12 +2,14 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 
+    @Modifying
     @Query("DELETE FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
     void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -158,11 +158,24 @@ public class CartServiceImpl implements CartService{
             throw new APIException("Product " + product.getProductName() + " not available in the cart!!!");
         }
 
-        cartItem.setProductPrice(product.getSpecialPrice());
-        cartItem.setQuantity(cartItem.getQuantity() + quantity);
-        cartItem.setDiscount(product.getDiscount());
-        cart.setTotalPrice(cart.getTotalPrice() + (cartItem.getProductPrice() * quantity));
-        cartRepository.save(cart);
+        // Calculate new quantity
+        int newQuantity = cartItem.getQuantity() + quantity;
+
+        // Validation to prevent negative quantities
+        if (newQuantity < 0) {
+            throw new APIException("The resulting quantity cannot be negative.");
+        }
+
+        if (newQuantity == 0){
+            deleteProductFromCart(cartId, productId);
+        } else {
+            cartItem.setProductPrice(product.getSpecialPrice());
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            cartItem.setDiscount(product.getDiscount());
+            cart.setTotalPrice(cart.getTotalPrice() + (cartItem.getProductPrice() * quantity));
+            cartRepository.save(cart);
+        }
+
         CartItem updatedItem = cartItemRepository.save(cartItem);
         if(updatedItem.getQuantity() == 0){
             cartItemRepository.deleteById(updatedItem.getCartItemId());
@@ -201,6 +214,7 @@ public class CartServiceImpl implements CartService{
     }
 
 
+    @Transactional
     @Override
     public String deleteProductFromCart(Long cartId, Long productId) {
         Cart cart = cartRepository.findById(cartId)
