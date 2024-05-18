32a6ca01409209e4b412b86ba51e0e007635f5ef
Faisal Memon <faisal.memon1991@gmail.com>
2024-05-18 11:43:56 +0530

    sbfs-249-models-order

---
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Order.java
@@ -0,0 +1,44 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "order", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDate orderDate;
+
+//    @OneToOne
+//    @JoinColumn(name = "payment_id")
+//    private Payment payment;
+
+    private Double totalAmount;
+    private String orderStatus;
+
+    // Reference to Address
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
---
+++ sb-ecom/src/main/java/com/ecommerce/project/model/OrderItem.java
@@ -0,0 +1,31 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "order_items")
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer quantity;
+    private double discount;
+    private double orderedProductPrice;
+
+}
