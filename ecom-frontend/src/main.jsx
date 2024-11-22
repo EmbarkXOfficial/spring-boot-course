f64a756a207da0a13daa566fc76ea4ca44cf6e72
Faisal Memon <faisal.memon1991@gmail.com>
2024-11-22 16:34:59 +0530

    24 Set Up Redux for Global State Management

--- ecom-frontend/src/main.jsx
+++ ecom-frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './store/reducers/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>,
 )
