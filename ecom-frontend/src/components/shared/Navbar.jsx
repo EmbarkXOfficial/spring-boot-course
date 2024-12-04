87cff0e8f47a6016bfca8c3557193a95f4e186ab
Faisal Memon <faisal.memon1991@gmail.com>
2024-12-04 20:39:05 +0530

    Building a Reactive Cart Badge

--- ecom-frontend/src/components/shared/Navbar.jsx
+++ ecom-frontend/src/components/shared/Navbar.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { FaShoppingCart, FaSignInAlt, FaStore } from "react-icons/fa";
 import { IoIosMenu } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
+    const { cart } = useSelector((state) => state.carts);
     
     return (
         <div className="h-[70px] bg-custom-gradient text-white z-50 flex items-center sticky top-0">
@@ -64,7 +66,7 @@ const Navbar = () => {
                     to="/cart">
                         <Badge
                             showZero
-                            badgeContent={0}
+                            badgeContent={cart?.length || 0}
                             color="primary"
                             overlap="circular"
                             anchorOrigin={{ vertical: 'top', horizontal: 'right', }}>
