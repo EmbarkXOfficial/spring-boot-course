0b3c5115516173aa361306d520ed6c98733b7ea0
Faisal Memon <faisal.memon1991@gmail.com>
2024-12-05 09:54:53 +0530

    Fixing Subtotal Price

--- ecom-frontend/src/components/cart/Cart.jsx
+++ ecom-frontend/src/components/cart/Cart.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import ItemContent from "./ItemContent";
 import CartEmpty from "./CartEmpty";
+import { formatPrice } from "../../utils/formatPrice";
 
 const Cart = () => {
     const dispatch = useDispatch();
@@ -10,7 +11,7 @@ const Cart = () => {
     const newCart = { ...cart };
 
     newCart.totalPrice = cart?.reduce(
-        (acc, cur) => acc * Number(cur?.specialPrice) * Number(cur?.quantity), 0
+        (acc, cur) => acc + Number(cur?.specialPrice) * Number(cur?.quantity), 0
     );
 
     if (!cart || cart.length === 0) return <CartEmpty />;
@@ -53,7 +54,7 @@ const Cart = () => {
                 <div className="flex text-sm gap-1 flex-col">
                     <div className="flex justify-between w-full md:text-lg text-sm font-semibold">
                         <span>Subtotal</span>
-                        <span>$400</span>
+                        <span>{formatPrice(newCart?.totalPrice)}</span>
                     </div>
 
                     <p className="text-slate-500">
