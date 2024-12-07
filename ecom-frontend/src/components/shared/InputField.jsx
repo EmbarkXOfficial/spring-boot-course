46262c9ca1ab6b3725b00f10546a417cf1bc55fd
Faisal Memon <faisal.memon1991@gmail.com>
2024-12-07 05:59:56 +0530

    Setting up InputField Component

---
+++ ecom-frontend/src/components/shared/InputField.jsx
@@ -0,0 +1,60 @@
+const InputField = ({
+    label,
+    id,
+    type,
+    errors,
+    register,
+    required,
+    message,
+    className,
+    min,
+    value,
+    placeholder,
+}) => {
+    return (
+        <div className="flex flex-col gap-1 w-full">
+            <label
+                htmlFor="id"
+                className={`${
+                    className ? className : ""
+                } font-semibold text-sm text-slate-800`}>
+                {label}
+            </label>
+            <input
+                type={type}
+                id={id}
+                placeholder={placeholder}
+                className={`${
+                    className ? className : ""
+                } px-2 py-2 border outline-none bg-transparent text-slate-800 rounded-md ${
+                    errors[id]?.message ? "border-red-500" : "border-slate-700" 
+                }`}
+                {...register(id, {
+                    required: {value: required, message},
+                    minLength: min
+                        ? { value: min, message: `Minimum ${min} character is required`}
+                        : null,
+                    pattern:
+                        type === "email"
+                            ? {
+                                value: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+com+$/,
+                                message: "Invalid email"
+                            }
+                            : type === "url"
+                            ? {
+                                value: /^(https?:\/\/)?(([a-zA-Z0-9\u00a1-\uffff-]+\.)+[a-zA-Z\u00a1-\uffff]{2,})(:\d{2,5})?(\/[^\s]*)?$/,
+                                message: "Please enter a valid url"
+                            }
+                            : null,
+
+                })}
+                />
+
+                {errors[id]?.message && (
+                    <p className="text-sm font-semibold text-red-600 mt-0">
+                        {errors[id]?.message}
+                    </p>
+                )}
+        </div>
+    );
+};
