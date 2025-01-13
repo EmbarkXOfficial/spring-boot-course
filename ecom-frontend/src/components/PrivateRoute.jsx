a06ccde87a34085dd327c43879f4c0f4e84f7b28
Faisal Memon <faisal.memon1991@gmail.com>
2025-01-13 19:42:36 +0530

    Redirecting User to Home After Login

--- ecom-frontend/src/components/PrivateRoute.jsx
+++ ecom-frontend/src/components/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = ({ publicPage = false }) => {
     const { user } = useSelector((state) => state.auth);
     if (publicPage) {
-        return user ? <Navigate to="/profile" /> : <Outlet />
+        return user ? <Navigate to="/" /> : <Outlet />
     }
     return user ? <Outlet /> : <Navigate to="/login" />;
 }
