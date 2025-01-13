9119d34088572d08c44c91d4da3d705695c24453
Faisal Memon <faisal.memon1991@gmail.com>
2025-01-13 12:56:48 +0530

    Implementing Private Routes for Authentication in React with Redux

--- ecom-frontend/src/store/reducers/store.js
+++ ecom-frontend/src/store/reducers/store.js
@@ -6,7 +6,7 @@ import { authReducer } from "./authReducer";
 
 const user = localStorage.getItem("auth")
     ? JSON.parse(localStorage.getItem("auth"))
-    : [];
+    : null;
 
 const cartItems = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
