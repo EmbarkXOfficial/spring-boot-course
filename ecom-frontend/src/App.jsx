db201a9a556406748345a5154d6d48f5c9c9cbe0
Faisal Memon <faisal.memon1991@gmail.com>
2025-01-27 14:03:50 +0530

    Address Selection - Completing the Address Flow

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -24,8 +24,11 @@ function App() {
           <Route path='/about' element={ <About />}/>
           <Route path='/contact' element={ <Contact />}/>
           <Route path='/cart' element={ <Cart />}/>
-          <Route path='/checkout' element={ <Checkout />}/>
         
+          <Route path='/' element={<PrivateRoute />}>
+            <Route path='/checkout' element={ <Checkout />}/>
+          </Route>
+
           <Route path='/' element={<PrivateRoute publicPage />}>
             <Route path='/login' element={ <LogIn />}/>
             <Route path='/register' element={ <Register />}/>
--- ecom-frontend/src/components/checkout/Checkout.jsx
+++ ecom-frontend/src/components/checkout/Checkout.jsx
@@ -1,15 +1,36 @@
-import { Step, StepLabel, Stepper } from '@mui/material';
+import { Button, Step, StepLabel, Stepper } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import AddressInfo from './AddressInfo';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserAddresses } from '../../store/actions';
+import toast from 'react-hot-toast';
 
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const dispatch = useDispatch();
-    const { address } = useSelector(
+    const { isLoading, errorMessage } = useSelector((state) => state.errors);
+    const { address, selectedUserCheckoutAddress } = useSelector(
         (state) => state.auth
     )
+    const paymentMethod = false;
+
+    const handleBack = () => {
+        setActiveStep((prevStep) => prevStep - 1);
+    };
+
+    const handleNext = () => {
+        if(activeStep === 0 && !selectedUserCheckoutAddress) {
+            toast.error("Please select checkout address before proceeding.");
+            return;
+        }
+
+        if(activeStep === 1 && (!selectedUserCheckoutAddress || !paymentMethod)) {
+            toast.error("Please select payment address before proceeding.");
+            return;
+        }
+        
+        setActiveStep((prevStep) => prevStep + 1);
+    };
 
     const steps = [
         "Address",
@@ -35,6 +56,41 @@ const Checkout = () => {
         <div className='mt-5'>
             {activeStep === 0 && <AddressInfo address={address} />}
         </div>
+
+        <div
+            className='flex justify-between items-center px-4 fixed z-50 h-24 bottom-0 bg-white left-0 w-full py-4 border-slate-200'
+            style={{ boxShadow: "0 -2px 4px rgba(100, 100, 100, 0.15)" }}>
+            <Button
+                variant='outlined'
+                disabled={activeStep === 0}
+                onClick={handleBack}>
+                    Back
+            </Button>
+
+            {activeStep !== steps.length - 1 && (
+                <button
+                    disabled={
+                        errorMessage || (
+                            (activeStep === 0 ? !selectedUserCheckoutAddress
+                                : activeStep === 1 ? !paymentMethod
+                                : false
+                            )
+                        )
+                    }
+                    className={`bg-customBlue font-semibold px-6 h-10 rounded-md text-white
+                       ${
+                        errorMessage ||
+                        (activeStep === 0 && !selectedUserCheckoutAddress) ||
+                        (activeStep === 1 && !paymentMethod)
+                        ? "opacity-60"
+                        : ""
+                       }`}
+                       onClick={handleNext}>
+                    Proceed
+                </button>
+            )} 
+        </div>
+        
     </div>
   );
 }
