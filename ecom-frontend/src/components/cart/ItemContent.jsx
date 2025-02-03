b2c0c7909145c5633375e985da579fbcb3408775
Faisal Memon <faisal.memon1991@gmail.com>
2025-02-03 17:46:30 +0530

    Fixes

--- ecom-frontend/src/components/cart/ItemContent.jsx
+++ ecom-frontend/src/components/cart/ItemContent.jsx
@@ -53,7 +53,7 @@ const ItemContent = ({
 
                 <div className="md:w-36 sm:w-24 w-12">
                     <img 
-                        src={image}
+                        src={`${import.meta.env.VITE_BACK_END_URL}/images/${image}`}
                         alt={productName}
                         className="md:h-36 sm:h-24 h-12 w-full object-cover rounded-md"/>
                 
